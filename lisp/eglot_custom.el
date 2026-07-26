b537337c2be124ac5c767cd91c3e64d466f81933
mingsiamz <mingsiamz@outlook.com>
2026-07-26 22:18:42 +0800

    fix eglot configury

--- lisp/eglot_custom.el
+++ lisp/eglot_custom.el
@@ -21,32 +21,6 @@
       flymake-no-changes-timeout 1.5
       eglot-events-buffer-size 0)
 
-(defun my/eglot-init-options (orig server)
-  (let ((opts (funcall orig server)))
-    (append (if (listp opts) opts nil)
-            (list :extendedClientCapabilities (list :classFileContentsSupport t)
-                  :settings (list :java (list :contentProvider (list :preferred "fernflower")
-                                               :references (list :includeDecompiledSources t)
-                                               :saveActions (list :organizeImports t)
-                                               :import (list :enabled t)))))))
-(advice-add 'eglot-initialization-options :around #'my/eglot-init-options)
-
-;; jdt:// 反编译
-(defvar eglot-jdt-cache (expand-file-name "jdt-cache" temporary-file-directory))
-(defun eglot-jdt-uri-to-path (orig uri)
-  (if (not (string-prefix-p "jdt://" uri))
-      (funcall orig uri)
-    (let ((file (expand-file-name (secure-hash 'md5 uri) eglot-jdt-cache)))
-      (unless (file-exists-p file)
-        (make-directory eglot-jdt-cache t)
-        (with-temp-file file
-          (insert (jsonrpc-request (eglot-current-server)
-                   :java/classFileContents (list :uri uri)))))
-      file)))
-(if (fboundp 'eglot-uri-to-path)
-    (advice-add 'eglot-uri-to-path :around #'eglot-jdt-uri-to-path)
-  (advice-add 'eglot--uri-to-path :around #'eglot-jdt-uri-to-path))
-
 ;; ---- DAP ----
 (use-package dap-mode
   :ensure t
